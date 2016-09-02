1bcbaf21378990a794b55cf071715da70aaafa04
dandesonjew <danieltanders@gmail.com>
2016-09-02 23:43:22 +0000

    nothing is broked

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,6 +1,6 @@
 class CoursesController < ApplicationController
   def index
-  	@search_term = params[:looking_for] || 'jhu'
+  	@search_term = params[:looking_for] || 'Math'
   	@courses = Coursera.for(@search_term)
   end
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,7 +2,7 @@ class Recipe
     include HTTParty
     
     ENV['FOOD2FORK_KEY'] = "8cd32e3d1712bd8a60dfb6760d0632e5"
-    hostport = ENV['FOOD2FORK_SERVER_AND_PORT'] || 'www.food2fork.com' 
+    hostport = 'www.food2fork.com' 
     base_uri "http://#{hostport}/api"
     default_params key: ENV['FOOD2FORK_KEY']
     format :json
